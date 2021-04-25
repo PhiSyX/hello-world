aacb7249b22586636efca2eb124de4dd576406c1
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2021-04-26 01:33:17 +0200

    refactor: nom des fichiers

--- ch03-common-programming-concepts/src/main.rs
+++ ch03-common-programming-concepts/src/main.rs
@@ -1,14 +1,14 @@
-mod s1_variables_mutability;
-use s1_variables_mutability::section_1;
+mod s01_variables_and_mutability;
+use s01_variables_and_mutability::section_1;
 
-mod s2_data_types;
-use s2_data_types::section_2;
+mod s02_data_types;
+use s02_data_types::section_2;
 
-mod s3_functions;
-use s3_functions::section_3;
+mod s03_how_functions_work;
+use s03_how_functions_work::section_3;
 
-mod s5_control_flow;
-use s5_control_flow::section_5;
+mod s05_control_flow;
+use s05_control_flow::section_5;
 
 fn main() {
   section_1();
--- ch04-understanding-ownership/src/main.rs
+++ ch04-understanding-ownership/src/main.rs
@@ -1,11 +1,11 @@
-mod s1_ownership;
-use s1_ownership::section_1;
+mod s01_what_is_ownership;
+use s01_what_is_ownership::section_1;
 
-mod s2_references_borrowing;
-use s2_references_borrowing::section_2;
+mod s02_references_n_borrowing;
+use s02_references_n_borrowing::section_2;
 
-mod s3_slice_type;
-use s3_slice_type::section_3;
+mod s03_slices;
+use s03_slices::section_3;
 
 fn main() {
   section_1();
