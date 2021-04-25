5225c5570c14b8e96dea927330572d4fc2c9e4b5
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2021-04-26 01:48:41 +0200

    Using Structs to Structure Related Data

---
+++ ch05-structs/src/main.rs
@@ -0,0 +1,14 @@
+mod s01_defining_structs;
+use s01_defining_structs::section_1;
+
+mod s02_example_structs;
+use s02_example_structs::section_2;
+
+mod s03_method_syntax;
+use s03_method_syntax::section_3;
+
+fn main() {
+  section_1();
+  section_2();
+  section_3();
+}
