f14bb4327c6e320ed727f781563a6c5aa40b989d
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2021-08-24 14:40:15 +0200

    refactor: lifetime

--- ch10-generics/src/s03_lifetime_syntax.rs
+++ ch10-generics/src/s03_lifetime_syntax.rs
@@ -1,21 +1,46 @@
 pub fn section_3() {
-    {
-        let r: u32;           // ---------+-- 'a
-                              //          |
-        {                     //          |
-            let x: u32 = 5;   // -+-- 'b  |
-            r = &x;           //  |       |
-        }                     // -+       |
-                              //          |
-        println!("r: {}", r); //          |
-    }
+    lifetime();
+}
 
-    {
-        let x: u8 = 5;        // ----------+-- 'b
-                              //           |
-        let r: &u8 = &x;      // --+-- 'a  |
-                              //   |       |
-        println!("r: {}", r); //   |       |
-                              // --+       |
+// fn _lifetime() {
+//     {
+//         let r: u32; // ---------+-- 'a
+//                     //          |
+//         {
+//             //          |
+//             let x: u32 = 5; // -+-- 'b  |
+//             r = &x; //  |       |
+//         } // -+       |
+//           //          |
+//         println!("r: {}", r); //          |
+//     }
+
+//     {
+//         let x: u8 = 5; // ----------+-- 'b
+//                        //           |
+//         let r: &u8 = &x; // --+-- 'a  |
+//                          //   |       |
+//         println!("r: {}", r); //   |       |
+//                               // --+       |
+//     }
+// }
+
+fn lifetime() {
+    let my_string = String::from("hello");
+    let my_string_2 = String::from("world");
+
+    let chosen = choice(&my_string, &my_string_2);
+    println!("chosen: {}", chosen);
+}
+
+fn choice<'a>(a: &'a str, b: &'a str) -> &'a str {
+    let c = "pwet";
+
+    if a.len() == b.len() {
+        c
+    } else if a.len() > b.len() {
+        a
+    } else {
+        b
     }
 }
