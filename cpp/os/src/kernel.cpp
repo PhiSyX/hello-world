df6e7d4fc9f40bcf8288b18ee9f710a563d50e77
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-02-02 23:57:24 +0100

    [C++] os: aliases de types

--- cpp/os/src/kernel.cpp
+++ cpp/os/src/kernel.cpp
@@ -1,7 +1,9 @@
+#include "types.hpp"
+
 void
 printf(char* str)
 {
-  static unsigned short* video_memory = (unsigned short*)0xb8000;
+  static u16* video_memory = (u16*)0xb8000;
 
   for (int i = 0; str[i] != '\0'; ++i) {
     video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
@@ -20,7 +22,7 @@ call_ctors()
 }
 
 extern "C" void
-kernel_main(void* multiboot_struct, unsigned int magicnumber)
+kernel_main(void* multiboot_struct, u32 magicnumber)
 {
   printf("Hello, World from kernel.cpp file!");
   while (1)
