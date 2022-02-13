b613c04236f61a5bc2dc4255cc668252d09b96e2
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-02-13 13:14:58 +0100

    [fortran]: fprettify

--- fortran/julia/mod.f90
+++ fortran/julia/mod.f90
@@ -1,27 +1,27 @@
 module mod
-    use, intrinsic::iso_fortran_env, only: DP => REAL64
-    implicit none
+   use, intrinsic::iso_fortran_env, only: DP => REAL64
+   implicit none
 
-    private
-    integer,            parameter :: MAX_ITERATION = 255
-    real   (kind = DP), parameter :: MAX_EPSLN = 2.0_DP
-    complex(kind = DP), parameter :: C = (-0.622772_DP, 0.42193_DP)
+   private
+   integer, parameter :: MAX_ITERATION = 255
+   real(kind=DP), parameter :: MAX_EPSLN = 2.0_DP
+   complex(kind=DP), parameter :: C = (-0.622772_DP, 0.42193_DP)
 
-    public :: iterate
+   public :: iterate
 contains
-    elemental function iterate(i0) result(n)
-        implicit none
+   elemental function iterate(i0) result(n)
+      implicit none
 
-        complex(kind = DP), &
-        intent(in)         :: i0
-        integer            :: n
-        complex(kind = DP) :: i
+      complex(kind=DP), &
+         intent(in)         :: i0
+      integer            :: n
+      complex(kind=DP) :: i
 
-        i = i0
-        n = 0
-        do while (abs(i) < MAX_EPSLN .and. n < MAX_ITERATION)
-            i = i**2 + C
-            n = n + 1
-        end do
-    end function iterate
+      i = i0
+      n = 0
+      do while (abs(i) < MAX_EPSLN .and. n < MAX_ITERATION)
+         i = i**2 + C
+         n = n + 1
+      end do
+   end function iterate
 end module mod
--- fortran/julia/openmp_set.f90
+++ fortran/julia/openmp_set.f90
@@ -1,116 +1,116 @@
 program openmp_set
-    use, intrinsic::iso_fortran_env, only: DP => REAL64, error_unit
-    use openmp_mod, only: compute
-    implicit none
+   use, intrinsic::iso_fortran_env, only: DP => REAL64, error_unit
+   use openmp_mod, only: compute
+   implicit none
 
-    complex(kind = DP), dimension(:, :), allocatable :: i
-    integer,            dimension(:, :), allocatable :: n
-    integer :: points
+   complex(kind=DP), dimension(:, :), allocatable :: i
+   integer, dimension(:, :), allocatable :: n
+   integer :: points
 
-    points = get_size()
+   points = get_size()
 
-    call initialize_i_values(i, points)
-    call initialize_n_values(n, points)
-    call compute(i, n)
-    call log_n_values(n)
+   call initialize_i_values(i, points)
+   call initialize_n_values(n, points)
+   call compute(i, n)
+   call log_n_values(n)
 
-    deallocate(i)
-    deallocate(n)
+   deallocate (i)
+   deallocate (n)
 contains
-    function get_size() result(n)
-        use, intrinsic::iso_fortran_env, only: error_unit
-        implicit none
+   function get_size() result(n)
+      use, intrinsic::iso_fortran_env, only: error_unit
+      implicit none
 
-        integer, parameter    :: DEFAULT_NUMBER = 100
-        character(len = 1024) :: buffer, io_msg
+      integer, parameter    :: DEFAULT_NUMBER = 100
+      character(len=1024) :: buffer, io_msg
 
-        integer :: n
-        integer :: io_stat
+      integer :: n
+      integer :: io_stat
 
-        if (command_argument_count() == 0) then
-            n = DEFAULT_NUMBER
-        else if (command_argument_count() == 1) then
-            call get_command_argument(1, buffer)
-            read (buffer, fmt = *, iostat = io_stat, iomsg = io_msg) n
-            if (io_stat /= 0) then
-                write (unit=error_unit, fmt='(2A)') &
-                    "Erreur:", io_msg
-                stop 2
-            end if
-        else
-            write (unit=error_unit, fmt='(A)') &
-                "Erreur:", "Nombre d'arguments incorrect"
-            stop 1
-        end if
-    end function get_size
-
-    subroutine initialize_i_values(i, points)
-        implicit none
-
-        complex(kind = DP), dimension(:, :), allocatable, &
-                intent(inout) :: i
-        integer, intent(in)   :: points
-
-        real(kind = DP), parameter :: MIN_RE = -1.8_DP, &
-                                      MAX_RE =  1.8_DP, &
-                                      MIN_IM = -1.8_DP, &
-                                      MAX_IM =  1.8_DP
-
-        real(kind = DP) :: delta_re, delta_im
-
-        integer :: c, d, status
-
-        allocate(i(points, points), stat = status)
-
-        if (status /= 0) then
-            write (unit=error_unit, fmt='(A)') &
-                "Erreur:", "Allocation de i impossible"
+      if (command_argument_count() == 0) then
+         n = DEFAULT_NUMBER
+      else if (command_argument_count() == 1) then
+         call get_command_argument(1, buffer)
+         read (buffer, fmt=*, iostat=io_stat, iomsg=io_msg) n
+         if (io_stat /= 0) then
+            write (unit=error_unit, fmt='(2A)') &
+               "Erreur:", io_msg
             stop 2
-        end if
+         end if
+      else
+         write (unit=error_unit, fmt='(A)') &
+            "Erreur:", "Nombre d'arguments incorrect"
+         stop 1
+      end if
+   end function get_size
 
-        delta_re = (MAX_RE - MIN_RE) / points
-        delta_im = (MAX_IM - MIN_IM) / points
+   subroutine initialize_i_values(i, points)
+      implicit none
 
-        do d = 1, size(i, 2)
-            do c = 1, size(i, 1)
-                i(c, d) = cmplx(MIN_RE + (c - 1)*delta_re, &
-                                MIN_IM + (d - 1)*delta_im, &
-                                kind = DP)
-            end do
-        end do
-    end subroutine initialize_i_values
+      complex(kind=DP), dimension(:, :), allocatable, &
+         intent(inout) :: i
+      integer, intent(in)   :: points
 
-    subroutine initialize_n_values(n, points)
-        implicit none
+      real(kind=DP), parameter :: MIN_RE = -1.8_DP, &
+                                  MAX_RE = 1.8_DP, &
+                                  MIN_IM = -1.8_DP, &
+                                  MAX_IM = 1.8_DP
 
-        integer, dimension(:, :), allocatable, &
-                intent(inout) :: n
-        integer, intent(in)   :: points
+      real(kind=DP) :: delta_re, delta_im
 
-        integer :: status
+      integer :: c, d, status
 
-        allocate(n(points, points), stat = status)
+      allocate (i(points, points), stat=status)
 
-        if (status /= 0) then
-            write (unit=error_unit, fmt='(A)') &
-                "Erreur:", "Allocation de i impossible"
-            stop 2
-        end if
+      if (status /= 0) then
+         write (unit=error_unit, fmt='(A)') &
+            "Erreur:", "Allocation de i impossible"
+         stop 2
+      end if
 
-        n = 0
-    end subroutine initialize_n_values
+      delta_re = (MAX_RE - MIN_RE)/points
+      delta_im = (MAX_IM - MIN_IM)/points
 
-    subroutine log_n_values(n)
-        implicit none
+      do d = 1, size(i, 2)
+         do c = 1, size(i, 1)
+            i(c, d) = cmplx(MIN_RE + (c - 1)*delta_re, &
+                            MIN_IM + (d - 1)*delta_im, &
+                            kind=DP)
+         end do
+      end do
+   end subroutine initialize_i_values
 
-        integer, dimension(:, :), intent(in) :: n
-        integer :: i
-        character(len = 80) :: fmt_str
+   subroutine initialize_n_values(n, points)
+      implicit none
 
-        write (fmt_str, '(A, I0, A)') '(', size(n, 2), 'I4)'
+      integer, dimension(:, :), allocatable, &
+         intent(inout) :: n
+      integer, intent(in)   :: points
 
-        do i = 1, size(n, 1)
-            print fmt_str, n(i, :)
-        end do
-    end subroutine log_n_values
+      integer :: status
+
+      allocate (n(points, points), stat=status)
+
+      if (status /= 0) then
+         write (unit=error_unit, fmt='(A)') &
+            "Erreur:", "Allocation de i impossible"
+         stop 2
+      end if
+
+      n = 0
+   end subroutine initialize_n_values
+
+   subroutine log_n_values(n)
+      implicit none
+
+      integer, dimension(:, :), intent(in) :: n
+      integer :: i
+      character(len=80) :: fmt_str
+
+      write (fmt_str, '(A, I0, A)') '(', size(n, 2), 'I4)'
+
+      do i = 1, size(n, 1)
+         print fmt_str, n(i, :)
+      end do
+   end subroutine log_n_values
 end program openmp_set
