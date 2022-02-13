eedda94d2acb03db605f36a88d15d91f260f7591
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-02-13 13:19:22 +0100

    [fortran]: calcule le plus grand diviseur commun de deux valeurs entières

---
+++ fortran/gcd.f90
@@ -0,0 +1,26 @@
+program compute_gcd
+   implicit none
+
+   integer, parameter :: MAX_VALUE = 10
+   integer :: i, j
+
+   do i = 1, MAX_VALUE
+      do j = 1, MAX_VALUE
+         print '(3I10)', i, j, gcd(i, j)
+      end do
+   end do
+contains
+   integer function gcd(i, j)
+      implicit none
+      integer, value :: i, j
+
+      do while (i /= j)
+         if (i > j) then
+            i = i - j
+         else if (j > i) then
+            j = j - i
+         end if
+      end do
+      gcd = i
+   end function gcd
+end program compute_gcd
