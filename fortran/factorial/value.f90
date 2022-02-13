895c23bcd27edb3b1f3d41175281d2e6246cef36
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-02-13 16:12:58 +0100

    [fortran]: appel par valeur

---
+++ fortran/factorial/value.f90
@@ -0,0 +1,20 @@
+program main
+   implicit none
+
+   integer :: n
+   n = 5
+   print *, factorial(n)
+contains
+   function factorial(n) result(fac)
+      implicit none
+
+      integer, value :: n
+      integer :: fac
+
+      fac = 1
+      do while (n > 1)
+         fac = fac*n
+         n = n - 1
+      end do
+   end function factorial
+end program main
