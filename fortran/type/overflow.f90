cac9027b0d6ea664a70331c01e98ec801f1af498
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-02-15 00:06:01 +0100

    [fortran]: débordement d'un nombre entier

---
+++ fortran/type/overflow.f90
@@ -0,0 +1,13 @@
+program main
+   use, intrinsic::iso_fortran_env, only: INT8
+   implicit none
+
+   integer(kind=INT8) :: val
+   integer :: i
+
+   val = 125
+   do i = 1, 6
+      print *, val
+      val = val + 1_INT8
+   end do
+end program main
