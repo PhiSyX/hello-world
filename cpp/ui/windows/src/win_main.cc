3cf86b66363b5a8c1694145e1e16703960afa356
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-06-02 14:15:59 +0200

    Animating the Backbuffer

--- cpp/ui/windows/src/win_main.cc
+++ cpp/ui/windows/src/win_main.cc
@@ -1,62 +1,100 @@
 #include <windows.h>
+#include <stdint.h>
 
 // @see https://docs.microsoft.com/en-us/windows/win32/api/winuser/
 
-#define internal static
-#define local_persist static
+#define internal        static
+#define local_persist   static
 #define global_variable static
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 // TODO: this is a global variable for now.
 
 global_variable bool running;
 
-global_variable BITMAPINFO bitmap_info;
-global_variable void* bitmap_memory;
-global_variable HBITMAP bitmap_handle;
-global_variable HDC bitmap_device_context;
+global_variable BITMAPINFO  bitmap_info;
+global_variable void*       bitmap_memory;
+global_variable int         bitmap_w;
+global_variable int         bitmap_h;
+
+global_variable int byte_per_pixel = 4;
 
 internal void
-win32_resize_di_section(int w, int h)
+render_weird_gradient(int offset_x, int offset_y) 
+{
+  int w = bitmap_w;
+  int h = bitmap_h;
+ 
+  int pitch = w * byte_per_pixel;
+  u8 * row = (u8 *)bitmap_memory;
+  for (int y = 0; y < bitmap_h; ++y) {
+    u32 * pixel = (u32 *)row;
+
+    for (int x = 0; x < bitmap_w; ++x) {
+        u8 blue = (x + offset_x);
+        u8 green = (y + offset_y);
+        /*
+          Memory:    BB GG RR xx
+          Register:  xx RR GG BB
+          
+          Pixel 32-bits
+        */
+        *pixel++ = ((green << 8) | blue);
+    }
+
+    row += pitch;
+  }
+}
+
+internal void
+win32_resize_dib_section(int w, int h)
 {
   // TODO: bulletproof this.
   // Maybe don't free first, free after, then free first if that fails.
 
-  if (bitmap_handle) {
-    DeleteObject(bitmap_handle);
+  if (bitmap_memory) {
+    VirtualFree(bitmap_memory, 0, MEM_RELEASE);
   }
 
-  if (bitmap_device_context) {
-    // TODO: should we recreate these under certain special circumstances
-    bitmap_device_context = CreateCompatibleDC(0);
-  }
+  bitmap_w = w;
+  bitmap_h = h;
 
-  bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
-  bitmap_info.bmiHeader.biWidth = w;
-  bitmap_info.bmiHeader.biHeight = h;
-  bitmap_info.bmiHeader.biPlanes = 1;
-  bitmap_info.bmiHeader.biBitCount = 32;
+  bitmap_info.bmiHeader.biSize        = sizeof(bitmap_info.bmiHeader);
+  bitmap_info.bmiHeader.biWidth       = bitmap_w;
+  bitmap_info.bmiHeader.biHeight      = -bitmap_h;
+  bitmap_info.bmiHeader.biPlanes      = 1;
+  bitmap_info.bmiHeader.biBitCount    = 32;
   bitmap_info.bmiHeader.biCompression = BI_RGB;
 
-  CreateDIBSection(
-    bitmap_device_context, &bitmap_info, DIB_RGB_COLORS, &bitmap_memory, 0, 0);
+  int bitmap_memory_size = (bitmap_w * bitmap_h) * byte_per_pixel;
+  bitmap_memory = VirtualAlloc(
+    0,
+    bitmap_memory_size,
+    MEM_COMMIT,
+    PAGE_READWRITE
+  );
 }
 
 internal void
-win32_update_window(HDC device_context, int x, int y, int w, int h)
+win32_update_window(HDC device_context, RECT *WindowRect,
+                    int x, int y, int w, int h)
 {
+  int window_w = WindowRect->right - WindowRect->left;
+  int window_h = WindowRect->bottom - WindowRect->top;
   StretchDIBits(device_context,
-                x,
-                y,
-                w,
-                h,
-                x,
-                y,
-                w,
-                h,
-                &bitmap_memory,
-                &bitmap_info,
-                DIB_RGB_COLORS,
-                SRCCOPY);
+                0, 0, bitmap_w, bitmap_h,
+                0, 0, window_w, window_h,
+                bitmap_memory, &bitmap_info,
+                DIB_RGB_COLORS, SRCCOPY);
 }
 
 LRESULT CALLBACK
@@ -68,43 +106,41 @@ MainWindowCallback(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_SIZE: {
       RECT client_rect;
       GetClientRect(window, &client_rect);
-
       auto w = client_rect.right - client_rect.left;
       auto h = client_rect.bottom - client_rect.top;
-
-      win32_resize_di_section(w, h);
-      OutputDebugStringA("WM_SIZE\n");
+      win32_resize_dib_section(w, h);
     } break;
 
     case WM_DESTROY: {
-      OutputDebugStringA("WM_DESTROY\n");
+      // TODO: handle this with a message to the user.
+      running = false;
       PostQuitMessage(0);
     } break;
 
     case WM_CLOSE: {
-      OutputDebugStringA("WM_CLOSE\n");
+      // TODO: handle this with as an error - recreate window.
+      running = false;
       PostQuitMessage(0);
     } break;
 
     case WM_ACTIVATEAPP: {
+      
       OutputDebugStringA("WM_ACTIVATEAPP\n");
     } break;
 
     case WM_PAINT: {
-      OutputDebugStringA("WM_PAINT\n");
       PAINTSTRUCT paint;
-
       HDC device_context = BeginPaint(window, &paint);
 
-      auto w = paint.rcPaint.right - paint.rcPaint.left;
-      auto h = paint.rcPaint.bottom - paint.rcPaint.top;
       auto x = paint.rcPaint.left;
       auto y = paint.rcPaint.top;
+      auto w = paint.rcPaint.right  - paint.rcPaint.left;
+      auto h = paint.rcPaint.bottom - paint.rcPaint.top;
 
-      //   PatBlt(device_context, x, y, w, h, WHITENESS);
+      RECT client_rect;
+      GetClientRect(window, &client_rect);
 
-      win32_update_window(device_context, x, y, w, h);
-
+      win32_update_window(device_context, &client_rect, x, y, w, h);
       EndPaint(window, &paint);
     } break;
 
@@ -125,30 +161,62 @@ WinMain(HINSTANCE hInstance,
   WNDCLASS win_class = {};
 
   // todo: check if HREDRAW/VREDRAW/OWNDC still matter
-  win_class.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-  win_class.lpfnWndProc = DefWindowProc;
-  win_class.hInstance = hInstance;
+  win_class.style         = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+  win_class.lpfnWndProc   = MainWindowCallback;
+  win_class.hInstance     = hInstance;
   win_class.lpszClassName = "CppWindowClass";
 
   if (RegisterClass(&win_class)) {
-    HWND window_handle = CreateWindowExA(0,
-                                         win_class.lpszClassName,
-                                         "Cpp window",
-                                         WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-                                         CW_USEDEFAULT,
-                                         CW_USEDEFAULT,
-                                         CW_USEDEFAULT,
-                                         CW_USEDEFAULT,
-                                         NULL,
-                                         NULL,
-                                         hInstance,
-                                         NULL);
+    HWND window_handle = CreateWindowExA(
+        0,
+        win_class.lpszClassName,
+        "Cpp Window",
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
 
     if (window_handle) {
-      MSG message;
-      while (GetMessage(&message, NULL, 0, 0) != 0) {
-        TranslateMessage(&message);
-        DispatchMessage(&message);
+      int offset_x = 0;
+      int offset_y = 0;
+
+      running = true;
+      while (running) {
+        
+        MSG message;
+
+        while (PeekMessage(&message, 0, 0, 0, PM_REMOVE)) {
+          if (message.message == WM_QUIT) {
+            running = false;
+          }
+
+          TranslateMessage(&message);
+          DispatchMessage(&message);
+        }
+
+        render_weird_gradient(offset_x, offset_y);
+
+        HDC device_context = GetDC(window_handle);
+        RECT client_rect;
+        GetClientRect(window_handle, &client_rect);
+
+        int window_w = client_rect.right - client_rect.left;
+        int window_h = client_rect.bottom - client_rect.top;
+
+        win32_update_window(
+          device_context,
+          &client_rect,
+          0, 0, window_w, window_h
+        );
+        ReleaseDC(window_handle, device_context);
+
+        ++offset_x;
       }
     }
   } else {
