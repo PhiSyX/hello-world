5fd2a000975859c8c8cd088e62947534f54677f7
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-08-08 02:30:16 +0200

    Rust: codes inutiles

--- rust/arch/hexagonal/src/domain/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod create_pokemon;
-pub mod entities;
-pub mod fetch_all_pokemons;
-mod fetch_pokemon;
--- rust/book/ch06-enums/src/s03_if_let.rs
+++
@@ -1,8 +0,0 @@
-pub fn section_3() {
-
-  let some_u8_value = Some(0u8);
-  if let Some(3) = some_u8_value {
-    println!("three");
-  }
-
-}
