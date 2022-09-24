84c786cc39c5f0fc442d3446cfb2ede3e9e53b81
PhiSyX <738010+PhiSyX@users.noreply.github.com>
2022-09-24 14:23:02 +0200

    AoC(MMXV): jour 10, partie 1

---
+++ AoC/MMXV/haskell/day10.hs
@@ -0,0 +1,34 @@
+include_bytes :: IO String
+include_bytes = readFile "../puzzles/day10.txt"
+
+look_and_say :: String -> String
+look_and_say = lookup_until ""
+
+lookup_until [] ( n : r ) = lookup_until [n] r
+lookup_until buffer @ ( last : _ ) ( n : r ) | last == n = lookup_until ( n : buffer ) r
+lookup_until buffer @ ( last : _ ) ( n : r ) | last /= n = (say buffer) ++ lookup_until [n] r
+lookup_until buffer [] = say buffer
+
+say :: String -> String
+say p = (show $ length p) ++ [(head p)]
+
+-- n times
+r' :: Int -> (a -> a) -> a -> a
+r' 0 _ p = p
+r' n f p = f $ r' (n - 1) f p
+t' :: Int -> String -> String
+t' n input = r' n look_and_say input
+
+-- Main
+
+main :: IO ()
+main = do
+    input <- include_bytes
+
+    let s = input
+
+    putStrLn $ "--- Day 10: Elves Look, Elves Say ---"
+
+    putStrLn $ "--- Part One ---"
+    let part01 = t' 40 s
+    putStrLn $ "\t Your puzzle answer is " ++ show (length part01)
