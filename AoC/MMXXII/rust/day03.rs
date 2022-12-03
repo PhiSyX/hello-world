c18827760ec7a39b122fb62eb34f26397addf38e
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-12-03 19:28:16 +0100

    AoC(2022): day03 / part02 (+nightly)

--- AoC/MMXXII/rust/day03.rs
+++ AoC/MMXXII/rust/day03.rs
@@ -1,5 +1,8 @@
+#![feature(iter_array_chunks)]
+
 use core::fmt;
-use std::{collections::HashSet, ops::Div};
+use core::ops::Div;
+use std::collections::HashSet;
 
 use mmxxii_rust::solved_output;
 
@@ -11,6 +14,10 @@ fn main() {
     let part01 = solve_part01(PUZZLE);
     println!("--- Part One ---");
     println!("\tYour puzzle answer is {}.", solved_output(part01));
+
+    let part02 = solve_part02(PUZZLE);
+    println!("--- Part Two ---");
+    println!("\tYour puzzle answer is {}.", solved_output(part02));
 }
 
 fn solve_part01(input: &'static str) -> impl fmt::Display {
@@ -20,6 +27,37 @@ fn solve_part01(input: &'static str) -> impl fmt::Display {
             let (l, r) = line.split_at(line.len().div(2));
             let [l, r] = [l, r].map(|p| p.chars().collect::<HashSet<_>>());
             l.intersection(&r).copied().collect::<Vec<_>>()
+        })
+        .filter_map(|ch| {
+            Some(match ch {
+                | 'a'..='z' => (ch as u8 - b'a' + 1) as usize,
+                | 'A'..='Z' => (ch as u8 - b'A' + 27) as usize,
+                | _ => return None,
+            })
+        })
+        .sum::<usize>()
+}
+
+fn solve_part02(input: &'static str) -> impl fmt::Display {
+    input
+        .lines()
+        .map(|line| line.chars().collect::<HashSet<_>>())
+        .array_chunks::<3>()
+        .flat_map(|bags| {
+            let mut bags = bags.iter();
+
+            bags.next()
+                .and_then(|b| {
+                    bags.next().map(|i| {
+                        b.intersection(&i).copied().collect::<HashSet<_>>()
+                    })
+                })
+                .and_then(|b| {
+                    bags.next().map(|i| {
+                        b.intersection(&i).copied().collect::<Vec<_>>()
+                    })
+                })
+                .unwrap()
         })
         .filter_map(|ch| {
             Some(match ch {
