88b7f0edc9f6d02d2af5bb43b706eaf18fb20552
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2022-12-04 20:11:34 +0100

    AoC(2022): day04 / part02

--- AoC/MMXXII/rust/day04.rs
+++ AoC/MMXXII/rust/day04.rs
@@ -20,13 +20,28 @@ impl std::str::FromStr for Range {
         let start = l.parse()?;
         let end = r.parse()?;
 
-        Ok(Range { start, end })
+        Ok(Self { start, end })
     }
 }
 
 impl Range {
-    fn contains(&self, other: &Range) -> bool {
+    fn contains(&self, other: &Self) -> bool {
         self.start <= other.start && self.end >= other.end
+    }
+
+    fn intersection(&self, other: &Self) -> Self {
+        let start = std::cmp::max(self.start, other.start);
+        let end = std::cmp::min(self.end, other.end);
+
+        if start <= end {
+            Self { start, end }
+        } else {
+            Self { start: 0, end: 0 }
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.end == 0 && self.start == 0
     }
 }
 
@@ -36,6 +51,10 @@ fn main() {
     let part01 = solve_part01(PUZZLE);
     println!("--- Part One ---");
     println!("\tYour puzzle answer is {}.", solved_output(part01));
+
+    let part02 = solve_part02(PUZZLE);
+    println!("--- Part Two ---");
+    println!("\tYour puzzle answer is {}.", solved_output(part02));
 }
 
 fn solve_part01(input: &'static str) -> impl fmt::Display {
@@ -49,6 +68,24 @@ fn solve_part01(input: &'static str) -> impl fmt::Display {
         })
         .fold(0, |mut acc, (r1, r2)| {
             if r1.contains(&r2) || r2.contains(&r1) {
+                acc += 1;
+            }
+            acc
+        })
+}
+
+fn solve_part02(input: &'static str) -> impl fmt::Display {
+    input
+        .lines()
+        .filter_map(|line| {
+            let Some((l, r)) = line.split_once(',') else {
+                return None;
+            };
+            l.parse::<Range>().ok().zip(r.parse::<Range>().ok())
+        })
+        .fold(0, |mut acc, (r1, r2)| {
+            let intersection = r1.intersection(&r2);
+            if !intersection.is_empty() {
                 acc += 1;
             }
             acc
