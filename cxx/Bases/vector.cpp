29675b173a99f0799ed9645cc767da5943201dcb
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2023-03-16 00:52:40 +0100

    [C++]: vector

---
+++ cxx/Bases/vector.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+void
+print(std::vector<int> arr)
+{
+	for (auto item : arr) {
+		std::cout << item << ' ';
+	}
+
+	std::cout << '\n';
+}
+
+int
+main()
+{
+	std::vector<int> my_vector;
+
+	my_vector.reserve(10);
+
+	for (int i = 0; i < 10; i += 1) {
+		std::cout << "Size: " << my_vector.size() << '\n';
+		std::cout << "Capacity: " << my_vector.capacity() << '\n';
+		my_vector.push_back(i);
+	}
+
+	return 0;
+}
