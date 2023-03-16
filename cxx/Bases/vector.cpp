440b5ce2a2f237d9e8f4a3c751c7252920d701ed
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2023-03-16 13:13:13 +0100

    [C++]: vector emplace_back

--- cxx/Bases/vector.cpp
+++ cxx/Bases/vector.cpp
@@ -11,18 +11,56 @@ print(std::vector<int> arr)
 	std::cout << '\n';
 }
 
+struct MyStruct
+{
+	int num;
+
+	MyStruct(int n)
+	  : num(n)
+	{
+		std::cout << "-------------\n";
+		std::cout << "Ctor\n";
+	}
+
+	MyStruct(const MyStruct& ms)
+	  : num(ms.num)
+	{
+		std::cout << "Copy " << ms.num << "\n";
+	}
+
+	MyStruct(MyStruct&& ms)
+	  : num(ms.num)
+	{
+		std::cout << "Move " << ms.num << "\n";
+	}
+};
+
 int
 main()
 {
-	std::vector<int> my_vector;
+	std::vector<int> my_vector1;
 
-	my_vector.reserve(10);
+	my_vector1.reserve(10);
 
 	for (int i = 0; i < 10; i += 1) {
-		std::cout << "Size: " << my_vector.size() << '\n';
-		std::cout << "Capacity: " << my_vector.capacity() << '\n';
-		my_vector.push_back(i);
+		std::cout << "Size: " << my_vector1.size() << '\n';
+		std::cout << "Capacity: " << my_vector1.capacity() << '\n';
+		my_vector1.push_back(i);
 	}
+
+	std::vector<MyStruct> my_vector2;
+	MyStruct ms1(40);
+	my_vector2.push_back(ms1);
+
+	MyStruct ms2(41);
+	my_vector2.push_back(std::move(ms2));
+
+	my_vector2.push_back(MyStruct(42));
+
+	MyStruct ms3(43);
+	my_vector2.push_back(ms3);
+
+	my_vector2.emplace_back(44);
 
 	return 0;
 }
