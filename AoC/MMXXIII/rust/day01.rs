2014f624a081442641d89d125b8ae170afb1a7d5
Mike 'PhiSyX' S <738010+PhiSyX@users.noreply.github.com>
2023-12-02 15:10:49 +0100

    AoC 2023: Day 01 (fmt)

--- AoC/MMXXIII/rust/day01.rs
+++ AoC/MMXXIII/rust/day01.rs
@@ -3,49 +3,53 @@ use mmxxiii_rust::{filter_map_line, solved_output};
 const PUZZLE: &str = include_str!("../puzzles/day01.txt");
 
 const NUMERAL_WORDS: [&str; 10] = [
-    "zero", "one", "two", "three", "four", "five", "six", "seven",
-    "eight", "nine",
+	"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
 
-fn solve_part01(input: &'static str) -> u32 {
-    filter_map_line(input, retrieves_first_and_last_digits).sum::<u32>()
+fn solve_part01(input: &'static str) -> u32
+{
+	filter_map_line(input, retrieves_first_and_last_digits).sum::<u32>()
 }
 
-fn retrieves_first_and_last_digits(line: &str) -> Option<u32> {
-    let mut it = line
-        .chars()
-        .filter(char::is_ascii_digit)
-        .map(|ch| (ch as u8 - b'0') as u32);
-    let f = it.next()?;
-    let l = it.last().unwrap_or(f);
-    Some(f * 10 + l)
+fn retrieves_first_and_last_digits(line: &str) -> Option<u32>
+{
+	let mut it = line
+		.chars()
+		.filter(char::is_ascii_digit)
+		.map(|ch| (ch as u8 - b'0') as u32);
+	let f = it.next()?;
+	let l = it.last().unwrap_or(f);
+	Some(f * 10 + l)
 }
 
-fn solve_part02(input: &'static str) -> u32 {
-    filter_map_line(input, parse_numeral_words).sum::<u32>()
+fn solve_part02(input: &'static str) -> u32
+{
+	filter_map_line(input, parse_numeral_words).sum::<u32>()
 }
 
-fn parse_numeral_words(line: &str) -> Option<u32> {
-    let mut line = String::from(line);
+fn parse_numeral_words(line: &str) -> Option<u32>
+{
+	let mut line = String::from(line);
 
-    for (n, word) in NUMERAL_WORDS.iter().enumerate() {
-        let s = format!("{}{}{}", &word[..1], n, &word[word.len() - 1..]);
-        line = line.replace(word, &s);
-    }
+	for (n, word) in NUMERAL_WORDS.iter().enumerate() {
+		let s = format!("{}{}{}", &word[..1], n, &word[word.len() - 1..]);
+		line = line.replace(word, &s);
+	}
 
-    retrieves_first_and_last_digits(&line)
+	retrieves_first_and_last_digits(&line)
 }
 
-fn main() {
-    println!("--- Day 1: Trebuchet?! ---");
+fn main()
+{
+	println!("--- Day 1: Trebuchet?! ---");
 
-    let part01 = solve_part01(PUZZLE);
-    println!("--- Part One ---");
-    println!("\tYour puzzle answer is {}.", solved_output(part01));
+	let part01 = solve_part01(PUZZLE);
+	println!("--- Part One ---");
+	println!("\tYour puzzle answer is {}.", solved_output(part01));
 
-    let part02 = solve_part02(PUZZLE);
-    println!("--- Part Two ---");
-    println!("\tYour puzzle answer is {}.", solved_output(part02));
+	let part02 = solve_part02(PUZZLE);
+	println!("--- Part Two ---");
+	println!("\tYour puzzle answer is {}.", solved_output(part02));
 }
 
 // ---- //
@@ -53,31 +57,38 @@ fn main() {
 // ---- //
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests
+{
+	use super::*;
 
-    #[test]
-
-    fn test_example_01_01() {
-        const PUZZLE: &str = "1abc2
+	#[test]
+	fn test_example_01_01()
+	{
+		const PUZZLE: &str = "
+1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet";
+treb7uchet
+";
 
-        let part01 = solve_part01(PUZZLE);
-        assert_eq!(part01, 142);
-    }
+		let part01 = solve_part01(PUZZLE.trim());
+		assert_eq!(part01, 142);
+	}
 
-    #[test]
-    fn test_example_01_02() {
-        const PUZZLE: &str = "two1nine
+	#[test]
+	fn test_example_01_02()
+	{
+		const PUZZLE: &str = "
+two1nine
 eightwothree
 abcone2threexyz
 xtwone3four
 4nineeightseven2
 zoneight234
-7pqrstsixteen";
-        let part02 = solve_part02(PUZZLE);
-        assert_eq!(part02, 281);
-    }
+7pqrstsixteen
+";
+
+		let part02 = solve_part02(PUZZLE.trim());
+		assert_eq!(part02, 281);
+	}
 }
